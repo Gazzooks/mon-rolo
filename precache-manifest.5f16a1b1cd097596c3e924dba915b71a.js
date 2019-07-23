19b8b6f25650cf20e700e330ed998dae4a297c89
Gazzooks <gazzooks@gmail.com>
2019-07-23 17:01:17 -0230

    Updates

---
+++ precache-manifest.5f16a1b1cd097596c3e924dba915b71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8c35ed4aa463c5fbf706360b0a2f0f",
+    "url": "/mon-rolo/index.html"
+  },
+  {
+    "revision": "e941b1e1c1402a770091",
+    "url": "/mon-rolo/static/css/main.2ef4560e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/mon-rolo/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e941b1e1c1402a770091",
+    "url": "/mon-rolo/static/js/main.d4ce09d4.chunk.js"
+  },
+  {
+    "revision": "689f1240a282964c25d1",
+    "url": "/mon-rolo/static/js/runtime~main.e72d7a61.js"
+  }
+]);
